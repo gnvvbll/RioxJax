6cd02929a3ca1b060dbcc4968f00768d79a7363a
gnvvbll <gnvvbll@gmail.com>
2020-07-21 11:36:49 -0400

    added menu, smoothscroll

--- javascript/custom.js
+++ javascript/custom.js
@@ -27,3 +27,8 @@ $(document).scroll(function(){
         });
     }
 });
+
+
+
+
+
---
+++ javascript/scrolling.js
@@ -0,0 +1,25 @@
+const navbarToggler = document.querySelector(".navbar-toggler");
+const navbarMenu = document.querySelectorAll(".navbar ul");
+const navbarLinks = document.querySelectorAll(".navbar a");
+
+navbarToggler.addEventListener("click", navbarTogglerClick);
+
+function navbarTogglerClick() {
+    navbarToggler.classList.toggle("open-navbar-toggler");
+    navbarMenu.classList.toggle("open");
+}
+
+navbarLinks.forEach(elem => elem.addEventListener("click", navbarLinkClick));
+
+function navbarLinkClick(event) {
+    smoothScroll(event);
+     
+    if(navbarMenu.classList.contains("open")){
+        navbarToggler.click();
+    }
+}
+
+function smoothScroll(event) {
+    const targetId = event.currentTarget.getAttribute("href");
+    console.log(targetId);
+}
